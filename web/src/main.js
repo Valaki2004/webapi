80b9d79f8f7bc592a0639813f71e4c90534b811e
vtg <varastothgergo@ktch.hu>
2024-04-25 10:46:40 +0200

    szerkesztés házi

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,7 @@
 const doc = {
     empBody: document.querySelector("#empBody"),
     addButton: document.querySelector("#addButton"),
+    idInput: document.querySelector("#id"),
     nameInput: document.querySelector("#name"),
     cityInput: document.querySelector("#city"),
     salaryInput: document.querySelector("#salary")
@@ -9,7 +10,8 @@ const state = {
     url: 'http://localhost:8000/employees',
     name: '',
     city: '',
-    salary: 300
+    salary: 300,
+    add: true
 }
 doc.addButton.addEventListener('click', () => {
     getdataFromForm()
@@ -55,15 +57,16 @@ function renderEmployees(employeeList) {
         <td>${emp.name}</td>
         <td>${emp.city}</td>
         <td>${emp.salary}</td>
-        <td><button class="btn btn-primary">Szerkesztés</button></td>
-        <td><button class="btn btn-danger" onclick="StartDelete(${emp.id})">Törlés</button></td>`
+        <td><button class="btn btn-primary"data-id="${emp.id}"data-name="${emp.name}"data-city="${emp.city}"data-salary="${emp.salary}" data-bs-toggle="modal" data-bs-target="#operatorModalonclick="startEdit(this)"><i class="bi bi-feather"></i>Szerkesztés</button></td>
+        <td><button class="btn btn-danger"onclick="StartDelete(${emp.id})">Törlés<i class="bi bi-trash3-fill"></i></button></td>`
         doc.empBody.appendChild(row)
     });    
 }
 function deleteModalContent() {
-    doc.nameInput.value= '',
-    doc.nameCity.value= '',
-    doc.nameSalary.value= ''
+    doc.idInput = ''
+    doc.nameInput.value = ''
+    doc.cityInput = ''
+    doc.salaryInput = ''
 }
 function clearTableContent(){
     doc.empBody.textContent= ''
@@ -76,5 +79,12 @@ function StartDelete(id){
 function deleteEmployee(id){
     let newUrl = state.url + '/'+ id
     fetch(newUrl, { method:'delete' })
+}
+function startEdit(source){
+    console.log("Szerkesztés...")
+    doc.idInput.value = source.dataset.id
+    doc.nameInput.value = source.dataset.name
+    doc.cityInput.value = source.dataset.city
+    doc.salaryInput.value = source.dataset.salary
 }
 getEmployees()
